46977019597af927f99ab96b9474bb586ffc1876
brumoreiras <bruhnno.ms@hotmail.com>
2023-05-26 09:43:21 -0300

    atualização do codigo

--- beecrowd1244.java
+++ beecrowd1244.java
@@ -5,9 +5,13 @@ public class beecrowd1244 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         int n = leia.nextInt();
-
-     for (int i = 0; i < n; i++) {
-            String[] conjuntoDeStrings = leia.nextLine().split(" ");
+        int cont = 0;
+        String[] conjuntoDeStrings = {};
+/*  
+     for (int i = 0; i < n; i++) { */
+        while(n < cont)
+            conjuntoDeStrings = leia.nextLine().split(" ");
+           
 
             for (int j = 1; j < conjuntoDeStrings.length; j++) {
 
@@ -22,9 +26,9 @@ public class beecrowd1244 {
                 
             }
             System.out.println(Arrays.toString(conjuntoDeStrings));
-        }
 
-        leia.close();
+            cont++;
+
+        }
     }
 
-}
