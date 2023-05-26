0363639cde43bbf6f19cad0824f13a2552ba5a85
brumoreiras <bruhnno.ms@hotmail.com>
2023-05-26 16:39:53 -0300

    Concluido

--- beecrowd1244.java
+++ beecrowd1244.java
@@ -1,33 +1,32 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class beecrowd1244 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        int n = leia.nextInt();
-        int cont = 0;
-        String[] conjuntoDeStrings = {};
-/*  
-     for (int i = 0; i < n; i++) { */
-        while(n < cont)
-            conjuntoDeStrings = leia.nextLine().split(" ");
-           
-            for (int j = 1; j < conjuntoDeStrings.length; j++) {
+        int n = Integer.parseInt(leia.nextLine());
 
-                int indice = j - 1;
-                String palavra = conjuntoDeStrings[j];
+        for (int q = 0; q < n; q++) {
+            String[] conjuntoDeStrings = leia.nextLine().split(" ");
 
-                while (indice > 0 && conjuntoDeStrings[indice].length() < palavra.length()) {
-                    conjuntoDeStrings[indice + 1] = conjuntoDeStrings[indice];
-                    indice--;
+            for (int i = 0; i < conjuntoDeStrings.length - 1; i++) {
+                for (int j = 0; j < conjuntoDeStrings.length - i - 1; j++) {
+                    if (conjuntoDeStrings[j].length() < conjuntoDeStrings[j + 1].length()) {
+                        String aux = conjuntoDeStrings[j];
+                        conjuntoDeStrings[j] = conjuntoDeStrings[j + 1];
+                        conjuntoDeStrings[j + 1] = aux;
+                    }
                 }
-                conjuntoDeStrings[indice + 1] = palavra;
-                
             }
-            System.out.println(Arrays.toString(conjuntoDeStrings));
 
-            cont++;
+            for (int i = 0; i < conjuntoDeStrings.length; i++) {
+                System.out.print(conjuntoDeStrings[i]);
+                if (i < conjuntoDeStrings.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
 
         }
+        leia.close();
     }
-
+}
